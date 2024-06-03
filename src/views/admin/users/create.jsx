b8257ef8e0f8ecde6f22faacadf5265af3e3fc67
alfanaisy <alfanaisy7@gmail.com>
2024-06-04 02:22:24 +0700

    Create user

--- src/views/admin/users/create.jsx
+++ src/views/admin/users/create.jsx
@@ -1,6 +1,36 @@
+import { useNavigate } from 'react-router-dom';
 import SidebarMenu from '../../../components/SidebarMenu';
+import Cookies from 'js-cookie';
+import { useState } from 'react';
+import apiClient from '../../../services/api';
 
+const token = Cookies.get('token');
 export default function UsersCreate() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [validation, setValidation] = useState([]);
+
+  const storeUser = async (e) => {
+    e.preventDefault();
+
+    apiClient.defaults.headers.common['Authorization'] = token;
+
+    await apiClient
+      .post('/api/admin/users', {
+        name,
+        email,
+        password,
+      })
+      .then(() => {
+        navigate('/admin/users');
+      })
+      .catch((error) => setValidation(error.response.data));
+  };
+
   return (
     <div className="container my-5">
       <div className="row">
@@ -10,7 +40,55 @@ export default function UsersCreate() {
         <div className="col-md-9">
           <div className="card border-0 rounded shadow-sm">
             <div className="card-header">ADD USER</div>
-            <div className="card-body">HALAMAN USER CREATE</div>
+            <div className="card-body">
+              {validation.errors && (
+                <div className="alert alert-danger mt-2 pb-0">
+                  {validation.errors.map((error, index) => (
+                    <p key={index}>
+                      {error.path} : {error.msg}
+                    </p>
+                  ))}
+                </div>
+              )}
+              <form onSubmit={storeUser}>
+                <div className="form-group mb-3">
+                  <label className="mb-1 fw-bold">Full Name</label>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    placeholder="Full Name"
+                  />
+                </div>
+
+                <div className="form-group mb-3">
+                  <label className="mb-1 fw-bold">Email address</label>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    placeholder="Email Address"
+                  />
+                </div>
+
+                <div className="form-group mb-3">
+                  <label className="mb-1 fw-bold">Password</label>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    placeholder="Password"
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-sm btn-primary">
+                  SAVE
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
